d75eca9569d77abf61273deafc8e0d0f0d497f6f
Nuno <Nuno@LAPTOP-0056K0NU>
2017-02-16 03:15:42 +0000

    Add project files.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.CostBox = new System.Windows.Forms.TextBox();
+            this.PaidBox = new System.Windows.Forms.TextBox();
+            this.Text5 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Text05 = new System.Windows.Forms.TextBox();
+            this.Text1 = new System.Windows.Forms.TextBox();
+            this.Text02 = new System.Windows.Forms.TextBox();
+            this.Text01 = new System.Windows.Forms.TextBox();
+            this.Text005 = new System.Windows.Forms.TextBox();
+            this.Text001 = new System.Windows.Forms.TextBox();
+            this.Text002 = new System.Windows.Forms.TextBox();
+            this.Text2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DimGray;
+            this.label1.Location = new System.Drawing.Point(42, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cost";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DimGray;
+            this.label2.Location = new System.Drawing.Point(42, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Paid";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(42, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "5 polaquinhos";
+            this.label3.Click += new System.EventHandler(this.label3_Click_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(42, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "2 polaquinhos";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(42, 186);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "1 polaquinho";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(42, 250);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "0.20 polaquitos";
+            this.label6.Click += new System.EventHandler(this.label6_Click_1);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(43, 218);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(98, 16);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "0.50 polaquitos";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(189, 122);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(98, 16);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "0.10 polaquitos";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(189, 154);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(98, 16);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "0.05 polaquitos";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(189, 218);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(91, 16);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "0.01 polaquito";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(189, 186);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(98, 16);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "0.02 polaquitos";
+            // 
+            // CostBox
+            // 
+            this.CostBox.Location = new System.Drawing.Point(137, 37);
+            this.CostBox.Name = "CostBox";
+            this.CostBox.Size = new System.Drawing.Size(100, 20);
+            this.CostBox.TabIndex = 11;
+            this.CostBox.Text = "0";
+            this.CostBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.CostBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // PaidBox
+            // 
+            this.PaidBox.Location = new System.Drawing.Point(137, 79);
+            this.PaidBox.Name = "PaidBox";
+            this.PaidBox.Size = new System.Drawing.Size(100, 20);
+            this.PaidBox.TabIndex = 12;
+            this.PaidBox.Text = "0";
+            this.PaidBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Text5
+            // 
+            this.Text5.Location = new System.Drawing.Point(140, 118);
+            this.Text5.Name = "Text5";
+            this.Text5.ReadOnly = true;
+            this.Text5.Size = new System.Drawing.Size(35, 20);
+            this.Text5.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(263, 77);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "Ok";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Text05
+            // 
+            this.Text05.Location = new System.Drawing.Point(140, 214);
+            this.Text05.Name = "Text05";
+            this.Text05.ReadOnly = true;
+            this.Text05.Size = new System.Drawing.Size(35, 20);
+            this.Text05.TabIndex = 23;
+            // 
+            // Text1
+            // 
+            this.Text1.Location = new System.Drawing.Point(140, 185);
+            this.Text1.Name = "Text1";
+            this.Text1.ReadOnly = true;
+            this.Text1.Size = new System.Drawing.Size(35, 20);
+            this.Text1.TabIndex = 24;
+            this.Text1.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // Text02
+            // 
+            this.Text02.Location = new System.Drawing.Point(140, 249);
+            this.Text02.Name = "Text02";
+            this.Text02.ReadOnly = true;
+            this.Text02.Size = new System.Drawing.Size(35, 20);
+            this.Text02.TabIndex = 25;
+            // 
+            // Text01
+            // 
+            this.Text01.Location = new System.Drawing.Point(293, 121);
+            this.Text01.Name = "Text01";
+            this.Text01.ReadOnly = true;
+            this.Text01.Size = new System.Drawing.Size(35, 20);
+            this.Text01.TabIndex = 26;
+            // 
+            // Text005
+            // 
+            this.Text005.Location = new System.Drawing.Point(293, 153);
+            this.Text005.Name = "Text005";
+            this.Text005.ReadOnly = true;
+            this.Text005.Size = new System.Drawing.Size(35, 20);
+            this.Text005.TabIndex = 27;
+            // 
+            // Text001
+            // 
+            this.Text001.Location = new System.Drawing.Point(293, 214);
+            this.Text001.Name = "Text001";
+            this.Text001.ReadOnly = true;
+            this.Text001.Size = new System.Drawing.Size(35, 20);
+            this.Text001.TabIndex = 29;
+            // 
+            // Text002
+            // 
+            this.Text002.Location = new System.Drawing.Point(293, 186);
+            this.Text002.Name = "Text002";
+            this.Text002.ReadOnly = true;
+            this.Text002.Size = new System.Drawing.Size(35, 20);
+            this.Text002.TabIndex = 30;
+            this.Text002.TextChanged += new System.EventHandler(this.textBox11_TextChanged);
+            // 
+            // Text2
+            // 
+            this.Text2.Location = new System.Drawing.Point(140, 153);
+            this.Text2.Name = "Text2";
+            this.Text2.ReadOnly = true;
+            this.Text2.Size = new System.Drawing.Size(35, 20);
+            this.Text2.TabIndex = 31;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(359, 278);
+            this.Controls.Add(this.Text2);
+            this.Controls.Add(this.Text002);
+            this.Controls.Add(this.Text001);
+            this.Controls.Add(this.Text005);
+            this.Controls.Add(this.Text01);
+            this.Controls.Add(this.Text02);
+            this.Controls.Add(this.Text1);
+            this.Controls.Add(this.Text05);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Text5);
+            this.Controls.Add(this.PaidBox);
+            this.Controls.Add(this.CostBox);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox CostBox;
+        private System.Windows.Forms.TextBox PaidBox;
+        private System.Windows.Forms.TextBox Text5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Text05;
+        private System.Windows.Forms.TextBox Text1;
+        private System.Windows.Forms.TextBox Text02;
+        private System.Windows.Forms.TextBox Text01;
+        private System.Windows.Forms.TextBox Text005;
+        private System.Windows.Forms.TextBox Text001;
+        private System.Windows.Forms.TextBox Text002;
+        private System.Windows.Forms.TextBox Text2;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double Cost = Convert.ToDouble(CostBox.Text);
+            double Paid= Convert.ToDouble(PaidBox.Text);
+            
+            if(Cost == 0 || Paid == 0)
+            {
+                MessageBox.Show("The Cost or Paid values cannot be equal to zero", "Incorrect cost/paid values",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            if (Cost > Paid)
+            {
+                MessageBox.Show("Pay more or get lost", "You idiot!",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                double changeLeft = Paid - Cost;
+                int d5 = 0; int d2 = 0; int d1 = 0; int d05 = 0; int d02 = 0;int d01 = 0;
+                int d005 = 0;int d002 = 0;int d001 = 0;
+                while (changeLeft >= 5)
+                {
+                    changeLeft = Math.Round(changeLeft - 5, 2);
+                    d5++;
+                    System.Diagnostics.Debug.WriteLine(changeLeft);
+                }
+                while (changeLeft >= 2)
+                {
+                    changeLeft = Math.Round(changeLeft - 2, 2);
+                    d2++;
+                    System.Diagnostics.Debug.WriteLine(changeLeft);
+                }
+                while (changeLeft >= 1)
+                {
+                    changeLeft = Math.Round(changeLeft - 1, 2);
+                    d1++;
+                    System.Diagnostics.Debug.WriteLine(changeLeft);
+                }
+                while (changeLeft >= 0.5)
+                {
+                    changeLeft = Math.Round(changeLeft - 0.5, 2);
+                    d05++;
+                    System.Diagnostics.Debug.WriteLine(changeLeft);
+                }
+                while (changeLeft >= 0.2)
+                {
+                    changeLeft = Math.Round(changeLeft - 0.2, 2);
+                    d02++;
+                    System.Diagnostics.Debug.WriteLine(changeLeft);
+                }
+                while (changeLeft >= 0.1)
+                {
+                    changeLeft = Math.Round(changeLeft - 0.1, 2);
+                    d01++;
+                    System.Diagnostics.Debug.WriteLine(changeLeft);
+                }
+                while (changeLeft >= 0.05)
+                {
+                    changeLeft = Math.Round(changeLeft - 0.05, 2);
+                    d005++;
+                    System.Diagnostics.Debug.WriteLine(changeLeft);
+                }
+                while (changeLeft >= 0.02)
+                {
+                    changeLeft = Math.Round(changeLeft - 0.02, 2);
+                    d002++;
+                    System.Diagnostics.Debug.WriteLine(changeLeft);
+                }
+                while (changeLeft >= 0.01)
+                {
+                    changeLeft = Math.Round(changeLeft - 0.01, 2);
+                    d001++;
+                    System.Diagnostics.Debug.WriteLine(changeLeft);
+                }
+                System.Diagnostics.Debug.WriteLine(changeLeft);
+                Text5.Text = d5.ToString();
+                Text2.Text = d2.ToString();
+                Text1.Text = d1.ToString();
+                Text05.Text = d05.ToString();
+                Text02.Text = d02.ToString();
+                Text01.Text = d01.ToString();
+                Text005.Text = d005.ToString();
+                Text002.Text = d002.ToString();
+                Text001.Text = d001.ToString();
+            }
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox11_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
